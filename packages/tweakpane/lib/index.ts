79ed7f5780b427a4130fe3e150f609669e35de38
cocopon <cocopon@me.com>
2021-05-10 04:50:31 +0900

    Split core module, #284

---
+++ packages/tweakpane/lib/index.ts
@@ -0,0 +1,9 @@
+import {Semver} from './misc/semver';
+
+export {ListBladeParams} from './blade/list/plugin';
+export {SliderBladeParams} from './blade/slider/plugin';
+export {TextBladeParams} from './blade/text/plugin';
+
+export {Pane} from './pane/pane';
+
+export const VERSION = new Semver('3.14.16');
